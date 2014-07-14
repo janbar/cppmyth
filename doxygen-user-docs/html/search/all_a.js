17746c413478243e628b8f6614c69addd6ce2ae9
janbar <jlbarriere68@gmail.com>
2014-07-14 13:49:29 +0200

    Update docs

--- doxygen-user-docs/html/search/all_a.js
+++ doxygen-user-docs/html/search/all_a.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['rcvbackendmessage',['RcvBackendMessage',['../classMyth_1_1ProtoEvent.html#aed27d737671cabf8f446477106ea3562',1,'Myth::ProtoEvent']]],
+  ['rcvversion',['RcvVersion',['../classMyth_1_1ProtoBase.html#a1cc04397dc559f4bca7e254aaf71ac61',1,'Myth::ProtoBase']]],
   ['readfield',['ReadField',['../classMyth_1_1ProtoBase.html#a35c5d200bb8c30b4e19d978654d6cf09',1,'Myth::ProtoBase']]],
   ['recording',['Recording',['../structMyth_1_1Recording.html',1,'Myth']]],
   ['recordingplayback',['RecordingPlayback',['../classMyth_1_1RecordingPlayback.html',1,'Myth']]],
--- doxygen-user-docs/html/search/functions_5.js
+++ doxygen-user-docs/html/search/functions_5.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['rcvbackendmessage',['RcvBackendMessage',['../classMyth_1_1ProtoEvent.html#aed27d737671cabf8f446477106ea3562',1,'Myth::ProtoEvent']]],
+  ['rcvversion',['RcvVersion',['../classMyth_1_1ProtoBase.html#a1cc04397dc559f4bca7e254aaf71ac61',1,'Myth::ProtoBase']]],
   ['readfield',['ReadField',['../classMyth_1_1ProtoBase.html#a35c5d200bb8c30b4e19d978654d6cf09',1,'Myth::ProtoBase']]],
   ['removerecordschedule',['RemoveRecordSchedule',['../classMyth_1_1WSAPI.html#acc8d1b160a40aae1bb8304c0e7a5967d',1,'Myth::WSAPI']]]
 ];
