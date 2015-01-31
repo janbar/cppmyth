037ea5f0a787efa4853d44080d9e28c6b6a7fa52
janbar <jlbarriere68@gmail.com>
2015-01-31 17:51:37 +0100

    Update docs

--- doxygen-dev-docs/html/search/all_3.js
+++ doxygen-dev-docs/html/search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['debug_5fctx_5ft',['debug_ctx_t',['../structdebug__ctx__t.html',1,'']]],
-  ['deleterecording',['DeleteRecording',['../classMyth_1_1Control.html#a7d19d668b88983fe08b5f20cc582e62d',1,'Myth::Control::DeleteRecording()'],['../classMyth_1_1WSAPI.html#a3b031d93d5077c0b5d24bfe0b41b18a6',1,'Myth::WSAPI::DeleteRecording()']]],
+  ['deleterecording',['DeleteRecording',['../classMyth_1_1Control.html#a7d19d668b88983fe08b5f20cc582e62d',1,'Myth::Control::DeleteRecording()'],['../classMyth_1_1WSAPI.html#a3b031d93d5077c0b5d24bfe0b41b18a6',1,'Myth::WSAPI::DeleteRecording(uint32_t chanid, time_t recstartts, bool forceDelete=false, bool allowRerecord=false)'],['../classMyth_1_1WSAPI.html#a5f613a76cc35dcc61144a6938ab0e70e',1,'Myth::WSAPI::DeleteRecording(uint32_t recordedid, bool forceDelete=false, bool allowRerecord=false)']]],
   ['disablerecordschedule',['DisableRecordSchedule',['../classMyth_1_1Control.html#a8568eb5b74d0202f76a2464faedb0f62',1,'Myth::Control::DisableRecordSchedule()'],['../classMyth_1_1WSAPI.html#aba17c17695ffe093b02e909e767b2449',1,'Myth::WSAPI::DisableRecordSchedule()']]]
 ];
