272c4a6123b8d8396e6671b4b5e56bf0b93b6ccd
janbar <jlbarriere68@gmail.com>
2025-06-27 11:27:18 +0200

    clean spaces from commit 8112cb5e95beb40939e4c5182ad2af3fc3cdc7a2

--- cppmyth/src/private/os/windows/winpthreads.c
+++ cppmyth/src/private/os/windows/winpthreads.c
@@ -499,7 +499,7 @@ int pthread_cancel(pthread_t t)
 #ifdef _M_X64
     ctxt.Rip = (uintptr_t) _pthread_invoke_cancel;
 #elif defined(_M_ARM64)
-    ctxt.Pc = (uintptr_t) _pthread_invoke_cancel; 
+    ctxt.Pc = (uintptr_t) _pthread_invoke_cancel;
 #else
     ctxt.Eip = (uintptr_t) _pthread_invoke_cancel;
 #endif
